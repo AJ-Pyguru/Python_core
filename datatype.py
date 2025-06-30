3eb3aba620acb7f3d674a7cb35ffec4776bc6188
AJ-Pyguru <varpedhananjayd400@gmail.com>
2025-06-30 23:05:19 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,37 @@
+# course="python"
+# print(course[1])
+# print(course[-4])
+
+
+# institute='the kiran acedemy'
+# print(institute[3])
+# print(institute[-4])
+# print(institute[4])
+# print(institute[8])
+# print(institute[-7])
+
+# name='dhananjay'
+# print(name[0:4:1])
+
+# name='dhananjay'
+# print(name[0:-6:1])
+
+# name='dhananjay'
+# print(name[-1:-6:-1])
+
+# name='gangasagar'
+# print(name[5:-1:-1])
+
+# institute='the kiran academy'
+# print(institute[0:4:1])
+
+# institute='the kiran academy'
+# print(institute[4:10:1])
+
+institute='the kiran academy'
+print(institute[-15:-18:-1])
+
+
+
+
+
